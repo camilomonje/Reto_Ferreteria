7b21a7922b6088c2a2ac8d350e8c02e800ee435c
camilomonje <lumonje12@gmail.com>
2022-03-30 16:24:52 -0500

    Primer commit - Se clonan repos

---
+++ server-side/retoFinalServerside/src/test/java/com/sofka/retoFinalServerside/RetoFinalServersideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.retoFinalServerside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoFinalServersideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
