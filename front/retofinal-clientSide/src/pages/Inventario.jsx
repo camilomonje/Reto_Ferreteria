a7dd3abaaf0d0cf6e49d1647ec7db924fb456e2f
camilomonje <lumonje12@gmail.com>
2022-03-31 15:51:09 -0500

    Se cambia el nombre a la carpeta client-side por front

---
+++ front/retofinal-clientSide/src/pages/Inventario.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventario = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Inventario;
---
+++ front/retofinal-clientSide/src/pages/Proovedores.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Proovedores = () => {
+    return (
+        <div>
+            <h1>Proovedores</h1>
+        </div>
+    );
+};
+
+export default Proovedores;
